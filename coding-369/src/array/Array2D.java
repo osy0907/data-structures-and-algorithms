32f30526069953c26b811926066a9b50c52230ba
Seungyong Oh <osy090712@gmail.com>
2021-09-02 18:39:13 +0900

    coding-369 - add 2D-Array

---
+++ coding-369/src/array/Array2D.java
@@ -0,0 +1,9 @@
+package array;
+
+public class Array2D {
+    public static void main(String[] args) {
+        Integer[][] data_list = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(data_list[0][1]);
+        System.out.println(data_list[1][1]);
+    }
+}
