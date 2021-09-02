34c904b9f581e558ad6548bb4513d21a232fe3b9
Seungyong Oh <osy090712@gmail.com>
2021-09-02 19:05:52 +0900

    coding-369 - array basic practice

--- coding-369/src/array/ArrayContainsString.java
+++ coding-369/src/array/ArrayContainsString.java
@@ -1,2 +1,48 @@
-package array;public class ArrayContainsString {
+package array;
+
+import java.util.Arrays;
+
+public class ArrayContainsString {
+    static String[] dataSet = {
+            "Braund, Mr. Owen Harris",
+            "Cumings, Mrs. John Bradley (Florence Briggs Thayer)",
+            "Heikkinen, Miss. Laina",
+            "Futrelle, Mrs. Jacques Heath (Lily May Peel)",
+            "Allen, Mr. William Henry",
+            "Moran, Mr. James",
+            "McCarthy, Mr. Timothy J",
+            "Palsson, Master. Gosta Leonard",
+            "Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)",
+            "Nasser, Mrs. Nicholas (Adele Achem)",
+            "Sandstrom, Miss. Marguerite Rut",
+            "Bonnell, Miss. Elizabeth",
+            "Saundercock, Mr. William Henry",
+            "Andersson, Mr. Anders Johan",
+            "Vestrom, Miss. Hulda Amanda Adolfina",
+            "Hewlett, Mrs. (Mary D Kingcome) ",
+            "Rice, Master. Eugene",
+            "Williams, Mr. Charles Eugene",
+            "Vander Planke, Mrs. Julius (Emelia Maria Vandemoortele)",
+            "Masselmani, Mrs. Fatima",
+            "Fynney, Mr. Joseph J",
+            "Beesley, Mr. Lawrence",
+            "McGowan, Miss. Anna",
+            "Sloper, Mr. William Thompson",
+            "Palsson, Miss. Torborg Danira",
+            "Asplund, Mrs. Carl Oscar (Selma Augusta Emilia Johansson)",
+            "Emir, Mr. Farred Chehab",
+            "Fortune, Mr. Charles Alexander",
+            "Dwyer, Miss. Ellen",
+            "Todoroff, Mr. Lalio"
+    };
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.stream(dataSet)
+                .filter(ArrayContainsString::isLookingForString)
+                .count());
+    }
+
+    private static boolean isLookingForString(String dataSet) {
+        return dataSet.contains("M");
+    }
 }
