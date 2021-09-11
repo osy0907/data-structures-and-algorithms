79a2d2ed47a0163d8c83f454ded41674cd64fe01
Seungyong Oh <osy090712@gmail.com>
2021-09-11 17:17:57 +0900

    fix - inner class

--- coding-369/src/tree/BinarySearchTree.java
+++ coding-369/src/tree/BinarySearchTree.java
@@ -1,16 +1,5 @@
 package tree;
 
-class Node{
-    Node left;
-    Node right;
-    int value;
-    Node(int data) {
-        this.value = data;
-        this.left = null;
-        this.right = null;
-    }
-}
-
 public class BinarySearchTree {
     Node head = null;
 
@@ -151,6 +140,18 @@ public class BinarySearchTree {
                 currNode = null;
             }
             return true;
+        }
+    }
+
+    public static class Node {
+        Node left;
+        Node right;
+        int value;
+
+        Node(int data) {
+            this.value = data;
+            this.left = null;
+            this.right = null;
         }
     }
 
