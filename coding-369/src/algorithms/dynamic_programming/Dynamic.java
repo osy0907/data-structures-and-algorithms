50fb30360db0ec63561161abb9e9633fc13f2534
Seungyong Oh <osy090712@gmail.com>
2021-09-15 21:09:55 +0900

    add dynamic-programming

---
+++ coding-369/src/algorithms/dynamic_programming/Dynamic.java
@@ -0,0 +1,18 @@
+package algorithms.dynamic_programming;
+
+public class Dynamic {
+    public int dynamicFunc(int data) {
+        Integer[] cache = new Integer[data + 1];
+        cache[0] = 0;
+        cache[1] = 1;
+        for (int index = 2; index < data + 1; index++) {
+            cache[index] = cache[index - 1] + cache[index - 2];
+        }
+        return cache[data];
+    }
+
+    public static void main(String[] args) {
+        Dynamic dynamic = new Dynamic();
+        System.out.println(dynamic.dynamicFunc(10));
+    }
+}
