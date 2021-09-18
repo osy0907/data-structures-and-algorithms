80d40b8f304d0ab56225396ce190bf14cb2de745
Seungyong Oh <osy090712@gmail.com>
2021-09-18 23:37:16 +0900

    add binary first search

---
+++ coding-369/src/algorithms/search/BFSSearch.java
@@ -0,0 +1,52 @@
+package algorithms.search;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class BFSSearch {
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<String>> graph = getGraph();
+        BFSSearch bObject = new BFSSearch();
+        System.out.println(bObject.bfsFunc(graph, "A"));
+    }
+
+    @NotNull
+    private static HashMap<String, ArrayList<String>> getGraph() {
+        HashMap<String, ArrayList<String>> graph = new HashMap<String, ArrayList<String>>();
+
+        graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
+        graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
+        graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
+        graph.put("D", new ArrayList<String>(Arrays.asList("B", "E", "F")));
+        graph.put("E", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("F", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("G", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("H", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("I", new ArrayList<String>(Arrays.asList("C", "J")));
+        graph.put("J", new ArrayList<String>(Arrays.asList("I")));
+        return graph;
+    }
+
+    public ArrayList<String> bfsFunc(HashMap<String, ArrayList<String>> graph, String startNode) {
+        ArrayList<String> visited = new ArrayList<String>();
+        ArrayList<String> needVisit = new ArrayList<String>();
+
+        needVisit.add(startNode);
+        int count = 0;
+
+        while (needVisit.size() > 0) {
+            count += 1;
+            String node = needVisit.remove(0);
+
+            if (!visited.contains(node)) {
+                visited.add(node);
+                needVisit.addAll(graph.get(node));
+            }
+        }
+        System.out.println(count);
+        return visited;
+    }
+}
