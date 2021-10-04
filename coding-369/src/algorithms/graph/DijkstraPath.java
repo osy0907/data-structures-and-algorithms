b266e6e19e5e0aa84765062ae3dba914b03358d5
Seungyong Oh <osy090712@gmail.com>
2021-10-04 09:35:48 +0900

    add DijkstraPath

---
+++ coding-369/src/algorithms/graph/DijkstraPath.java
@@ -0,0 +1,84 @@
+package algorithms.graph;
+
+import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class DijkstraPath {
+
+    public static void main(String[] args) {
+        DijkstraPath dijkstraPath = new DijkstraPath();
+        System.out.println(dijkstraPath.dijkstraFunc(getGraph(), "A"));
+    }
+
+    private static HashMap<String, ArrayList<Edge>> getGraph() {
+        HashMap<String, ArrayList<Edge>> graph = new HashMap<String, ArrayList<Edge>>();
+        graph.put("A", new ArrayList<Edge>(Arrays.asList(new Edge(8, "B"), new Edge(1, "C"), new Edge(2, "D"))));
+        graph.put("B", new ArrayList<Edge>());
+        graph.put("C", new ArrayList<Edge>(Arrays.asList(new Edge(5, "B"), new Edge(2, "D"))));
+        graph.put("D", new ArrayList<Edge>(Arrays.asList(new Edge(3, "E"), new Edge(5, "F"))));
+        graph.put("E", new ArrayList<Edge>(Arrays.asList(new Edge(1, "F"))));
+        graph.put("F", new ArrayList<Edge>(Arrays.asList(new Edge(5, "A"))));
+        return graph;
+    }
+
+    public HashMap<String, Integer> dijkstraFunc(HashMap<String, ArrayList<Edge>> graph, String start) {
+        Edge edgeNode, adjacentNode;
+        ArrayList<Edge> nodeList;
+        int currentDistance, weight, distance;
+        String currentNode, adjacent;
+
+        HashMap<String, Integer> distances = new HashMap<String, Integer>();
+        for (String key : graph.keySet()) {
+            distances.put(key, Integer.MAX_VALUE);
+        }
+        distances.put(start, 0);
+
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<Edge>();
+        priorityQueue.add(new Edge(distances.get(start), start));
+
+        while (priorityQueue.size() > 0) {
+            edgeNode = priorityQueue.poll();
+            currentDistance = edgeNode.distance;
+            currentNode = edgeNode.vertex;
+
+            if (distances.get(currentNode) < currentDistance) {
+                continue;
+            }
+
+            nodeList = graph.get(currentNode);
+            for (int index = 0; index < nodeList.size(); index++) {
+                adjacentNode = nodeList.get(index);
+                adjacent = adjacentNode.vertex;
+                weight = adjacentNode.distance;
+                distance = currentDistance + weight;
+
+                if (distance < distances.get(adjacent)) {
+                    distances.put(adjacent, distance);
+                    priorityQueue.add(new Edge(distance, adjacent));
+                }
+            }
+        }
+        return distances;
+    }
+
+    static class Edge implements Comparable<Edge> {
+        public int distance;
+        public String vertex;
+
+        public Edge(int distance, String vertex) {
+            this.distance = distance;
+            this.vertex = vertex;
+        }
+
+        public String toString() {
+            return "vertex: " + this.vertex + ", distance: " + this.distance;
+        }
+
+        @Override
+        public int compareTo(Edge edge) {
+            return this.distance - edge.distance;
+        }
+    }
+}
